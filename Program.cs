5835320f1d3e3575a00198e774909c27a9d07bab
zaydh1002 <abdulfatah@students.amikom.ac.id>
2022-03-23 23:29:05 +0700

    Inisialisasi project

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi2 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Taxiku driver = new Taxiku();
+
+            driver.DriverName   = "Fatah";
+            driver.Onduty       = true;
+            driver.NumPassanger = 21114327;
+
+            driver.TaxiInfo();
+            Console.WriteLine();
+            driver.PickUpPassanger();
+            driver.DropoffPassanger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxiku.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi2
+{
+    class Taxiku
+    {
+        public string DriverName {get; set;}
+        public bool Onduty {get; set;}
+        public int NumPassanger {get; set;}
+
+        public void TaxiInfo()
+    {
+            string duty;
+
+            if (Onduty)
+            {
+                duty = "Yes";
+            }
+            else
+            {
+                duty = "No";
+            }
+        Console.WriteLine("DriverName   : {0}", DriverName);
+        Console.WriteLine("OnDuty       : {0}", Onduty);
+        Console.WriteLine("NumPassanger : {0}", NumPassanger);
+    }
+    public void PickUpPassanger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", DriverName );
+        }
+    public void DropoffPassanger()
+    {
+        Console.WriteLine("{0} Selesai Mengantar Penumpang\n", DriverName);
+    }
+    }
+}
